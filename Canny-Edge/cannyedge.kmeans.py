0ecc989ab44006b7177acf72383c3289fdec33b9
mayribeiro15 <47997138+mayribeiro15@users.noreply.github.com>
2021-07-01 00:23:31 -0300

    Edit cannyedge

--- Canny-Edge/cannyedge.kmeans.py
+++ Canny-Edge/cannyedge.kmeans.py
@@ -34,14 +34,14 @@ blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
 #aplicando o filtro gaussiano
 cv2.imshow("Original", image)
-cv2.imshow("Blurred", blurred)
 
 canny_image = cv2.Canny(blurred, 30, 150) #tight 240,250 / mid 30,150 / wide 10,200
+image[canny_image != 0] = [0,255,0]
 
-cv2.imshow("Canny Edge Map", canny_image)
+cv2.imshow("Canny Edge", image)
 cv2.waitKey(0)
 
 filename2 = "idaho_cannyedge_kmeans.jpg"
-cv2.imwrite(filename2,canny_image)
+cv2.imwrite(filename2,image)
 
 
